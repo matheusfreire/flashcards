53b2209961b18edfe6b67757e42a44d6f1773bea
Matheus Freire <matheus.sfreire@gmail.com>
2018-03-20 00:17:34 -0300

    Add card in list

--- components/ListDecks.js
+++ components/ListDecks.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
-import { List, ListItem } from 'react-native-elements'
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+import { List, ListItem, Card } from 'react-native-elements'
 import { StackNavigator } from 'react-navigation'
+import { Ionicons } from '@expo/vector-icons'
+
 import { AppLoading } from 'expo'
 import { connect } from 'react-redux'
 
@@ -9,6 +11,7 @@ import { fetchDecks } from '../utils/api'
 import { getDecks } from '../actions'
 import DeckCard from './DeckCard'
 
+import { blue, white, purple } from '../utils/colors'
 
 class ListDecks extends Component {
 
@@ -54,11 +57,29 @@ class ListDecks extends Component {
             <View style={styles.container}>
                 <List>
                     {Object.keys(decks).map((key) => (
-                        <ListItem key={key}
-                            onPress={() => {alert(`${JSON.parse(decks[key]).title} cards`)}}
-                            title={JSON.parse(decks[key]).title}
-                            subtitle={`${JSON.parse(decks[key]).questions.length} cards`}
-                        />
+
+                        <Card key={key} style={styles.card} title={JSON.parse(decks[key]).title}>
+                            <Text style={{ marginBottom: 10, textAlign: 'center' }}>
+                                {`${JSON.parse(decks[key]).questions.length} cards`}
+                            </Text>
+
+                            <TouchableOpacity
+                                icon={
+                                    <Ionicons
+                                    name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
+                                    size={100}
+                                  />
+                                }
+                                style={styles.buttonStyle}
+                                onPress={() => { alert(`${JSON.parse(decks[key]).title} cards`) }} >
+                                <Text style={styles.submitBtnText}>View now</Text>
+                            </TouchableOpacity>
+                        </Card>
+                        // <ListItem key={key}
+                        //     onPress={() => { alert(`${JSON.parse(decks[key]).title} cards`) }}
+                        //     title={JSON.parse(decks[key]).title}
+                        //     subtitle={`${JSON.parse(decks[key]).questions.length} cards`}
+                        // />
                     ))}
                 </List>
             </View >
@@ -69,8 +90,23 @@ class ListDecks extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: white,
+    },
+    card: {
+        borderRadius: 5
+    },
+    buttonStyle:{
+        padding: 10,
+        borderRadius: 7,
+        height: 45,
+        marginLeft: 40,
+        marginRight: 40,
+        backgroundColor: purple
+    },
+    submitBtnText: {
+        color: white,
+        fontSize: 18,
+        textAlign: 'center',
     }
 })
 
